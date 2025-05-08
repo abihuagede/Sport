81b635664513e9e9d85404bb01de8ec2f39a12e2
abihuagede <abihuagede12gmail.com>
2025-05-08 05:03:46 -1200

    update

--- 442_home.js
+++ 442_home.js
@@ -1,6 +1,6 @@
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
 
 hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('active');
+  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
 });
