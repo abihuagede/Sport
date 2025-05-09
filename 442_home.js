ad1b7087e633c4064dd1341482f92c0c7cff631f
abihuagede <abihuagede12gmail.com>
2025-05-08 20:42:11 -1200

    new changes made

--- 442_home.js
+++ 442_home.js
@@ -1,6 +1,6 @@
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener('click', () => {
-  navLinks.style.display = navLinks.style.display === 'flex' ? 'none' : 'flex';
+hamburger.addEventListener("click", () => {
+  navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
 });
