093abed7e516a23703e8d23f6fc4eeedb10d8e4e
abihuagede <abihuagede12gmail.com>
2025-05-08 23:45:55 -1200

    new changes made

--- 442_home.js
+++ 442_home.js
@@ -4,3 +4,6 @@ const navLinks = document.querySelector(".nav-links");
 hamburger.addEventListener("click", () => {
   navLinks.style.display = navLinks.style.display === "flex" ? "none" : "flex";
 });
+function Regis_Form() {
+  window.location.href = "442_register.html";
+}
