8906938587d4110b4e7cead1b3964c362285fa20
abihuagede <abihuagede12gmail.com>
2025-05-11 02:15:14 -1200

    new component add

--- 442_register.js
+++ 442_register.js
@@ -1,3 +1,6 @@
 function Association_Gate() {
   window.location.href = "Association_Registration.html";
 }
+function Football_Clubs() {
+  window.location.href = "Football_Club.html";
+}
--- Association_Registration.js
+++ Association_Registration.js
@@ -39,3 +39,18 @@ window.addEventListener("load", function () {
     }
   });
 });
+const inputs = document.querySelectorAll(".code-input");
+
+inputs.forEach((input, index) => {
+  input.addEventListener("input", (e) => {
+    if (e.target.value.length === 1 && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace" && e.target.value.length === 0 && index > 0) {
+      inputs[index - 1].focus();
+    }
+  });
+});
